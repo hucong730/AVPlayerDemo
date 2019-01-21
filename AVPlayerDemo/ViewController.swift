e69d6cd16e483ec8a094b69fbb57d8d7d42ec19c
胡聪 <1753436588@qq.com>
2019-01-21 16:52:40 +0800

    Initial Commit

---
+++ AVPlayerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVPlayerDemo
+//
+//  Created by 胡聪 on 2019/1/21.
+//  Copyright © 2019 胡聪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
